ed2776dfb222d91ec98f3f26fc5579f55b96b8bb
Noemi1 <54690296+Noemi1@users.noreply.github.com>
2022-06-21 22:01:15 -0300

    aaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModoEscuro } from './helpers/modo-escuro';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'planner';
+export class AppComponent implements OnInit {
+  
+  modoEscuroAtivado = false;
+
+  constructor(
+    private modoEscuro: ModoEscuro
+  ) {
+    this.modoEscuro.ativado.subscribe(res => this.modoEscuroAtivado = res);
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,14 @@ export class HomeComponent implements OnInit {
     { id: 0, baixissimo: 50, baixo: 50, moderado: 50, arrojado: 50, superArrojado: 50, hedge: 50, total: 0, tipo: 'Sugestão' },
     { id: 1, baixissimo: 50, baixo: 50, moderado: 50, arrojado: 50, superArrojado: 50, hedge: 50, total: 0, tipo: 'Plano' },
   ];
+
+  perfilInvestivor = [
+    { id: 1, nome: 'Super Conservador (até 20% do patrimônio em risco)' },
+    { id: 2, nome: 'Conservador (de 20% à 30% do patrimônio em risco)' },
+    { id: 3, nome: 'Moderado (de 30% à 50% do patrimônio em risco)' },
+    { id: 4, nome: 'Arrojado (de 50% à 80% do patrimônio em risco)' },
+    { id: 5, nome: 'Super Arrojado (100% do patrimônio em risco)' },
+  ]
   constructor() { }
 
   ngOnInit(): void {
